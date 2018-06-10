0930af500e02627059a13302cba17ab6d1ee2d4d
Antonio Cortes <antoni_anemi@hotmail.com>
2018-06-10 15:25:16 -0700

    autentication integrated with firebase.

--- landscaperonline/landscaperonline/ViewController.swift
+++ landscaperonline/landscaperonline/ViewController.swift
@@ -4,17 +4,21 @@
 //
 //  Created by Antonio Cortes on 6/10/18.
 //  Copyright © 2018 antoniocortes. All rights reserved.
-//
-
 import UIKit
-
+import Firebase
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txt_username: UITextField!
+    @IBOutlet weak var txt_password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func logIn(_ sender: Any) {
+        guard let username = txt_username.text,let password = txt_password.text else{ return }
+        Auth.auth().signIn(withEmail: username, password: password) { (user, error) in
+            if error != nil{
+                return
+            }
+        }
+    }
 }
 
