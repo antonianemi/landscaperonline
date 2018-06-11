e3336965f9c3baaa07c3d871af2b28f70ed9d8a9
Antonio Cortes <antoni_anemi@hotmail.com>
2018-06-10 22:54:08 -0700

    SingUp integration with firebase

--- landscaperonline/landscaperonline/VCSingUP.swift
+++ landscaperonline/landscaperonline/VCSingUP.swift
@@ -12,15 +12,24 @@ class VCSingUP: UIViewController {
     @IBOutlet weak var txt_password: UITextField!
     @IBOutlet weak var txt_confirm_password: UITextField!
     @IBOutlet weak var btn_SingUp: UIButton!
-    
     @IBAction func btn_sing_up(_ sender: Any) {
+        guard let email = txt_email.text,let password = txt_password.text, let password2 = txt_confirm_password.text else { return }
+        
+        if password2 != password{
+            //error the password does not match
+            return
+        }
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if error != nil{
+            
+                return
+            }
+            let profile = user?.phoneNumber
+        }
         
     }
-    
-    
 }
 extension VCSingUP{
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         btn_SingUp.setFirstStyle()
